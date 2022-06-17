60342c24166a77102acd937b8f8766aa13ac646a
bornterra <eghae66@gmail.com>
2022-06-17 11:21:04 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://86e2492a.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
